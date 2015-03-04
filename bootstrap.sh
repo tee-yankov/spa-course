dcb1a7e7faebc03f19d3ad1f17493559b904fd68
Theodore Yankov <venoma333@gmail.com>
2015-03-04 14:53:49 +0200

    modify vagrant configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,6 @@ sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -y install nodejs
 cd /vagrant
-sudo npm install -g bower
+sudo npm install -g bower gulp
 npm install
 bower install --allow-root --config.interactive=false
-sudo npm install -g forever
-forever start server/server.js
