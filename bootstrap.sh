4eca0b4d9414b5d5ed5d0938e4e27b58f5bfc1ca
Theodore Yankov <venoma333@gmail.com>
2015-03-04 11:59:04 +0200

    modify vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y python-software-properties
+sudo apt-get install -y python-software-properties git
 sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -y install nodejs
